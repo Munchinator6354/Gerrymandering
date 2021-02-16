ca6ade3a7ee4501b8d268babb0a610590056ec4a
Ryan Isaacson <53586947+Munchinator6354@users.noreply.github.com>
2021-02-15 17:39:12 -0800

    Initial Commit

---
+++ Gerrymandering.java
@@ -0,0 +1,7 @@
+// Ryan Isaacson, CS&141, Winter 2021
+// Programming Assignment #5, Gerrymandering 02/15/2021
+//
+// This program allows you to search through data about
+// congressional voting districts and determine whether a 
+// particular state is gerrymandered.
+
